05cdc9d1536fb879280df677ae829654e5382077
Sophia Contesini <sophiamarques214@gmail.com>
2023-09-25 18:19:14 -0300

    feat: include header component

--- src/app/components/header/Header.tsx
+++ src/app/components/header/Header.tsx
@@ -6,8 +6,8 @@ export function Header() {
   return (
     <div className='header'>
       <div>
-        <h1>Hi, i'm Sophia! 👋</h1>
-        <h2>Software Engineer</h2>
+        <h1>Hello, i'm Sophia!</h1>
+        <h2>Front-end Developer / UX Designer</h2>
       </div>
       <Image src='/me.jpg' alt='Me Pic' width={325} height={310} priority />
     </div>
